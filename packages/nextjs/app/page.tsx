c88329ab1e4d727b8a4dedd685b6637630e0064c
Shiv Bhonde <shivbhonde04@gmail.com>
2024-04-19 13:49:38 +0530

    update page.tsx to show contract interaction

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,18 +1,45 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
+import { Address, AddressInput, InputBase } from "~~/components/scaffold-eth";
+import { useScaffoldReadContract, useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
+  const [newGreeting, setGreeting] = useState("");
+  const [counterAddress, setCounterAddress] = useState("");
+
+  const { data: totalCounter } = useScaffoldReadContract({
+    contractName: "YourContract",
+    functionName: "totalCounter",
+  });
+
+  const { data: userGreetingCounter, refetch: fetchUserGreetingCounter } = useScaffoldReadContract({
+    contractName: "YourContract",
+    functionName: "userGreetingCounter",
+    args: [counterAddress],
+    // only fetch when user click read button
+    query: { enabled: false },
+  });
+
+  const { writeContractAsync } = useScaffoldWriteContract("YourContract");
+
+  const handleSetGreeting = async () => {
+    try {
+      await writeContractAsync({ functionName: "setGreeting", args: [newGreeting] });
+    } catch (e) {
+      console.log("Error setting greeting", e);
+    }
+  };
 
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
+        <div className="px-5 flex flex-col items-center space-y-4">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
@@ -21,6 +48,28 @@ const Home: NextPage = () => {
             <p className="my-2 font-medium">Connected Address:</p>
             <Address address={connectedAddress} />
           </div>
+          <div className="card w-96 bg-base-100 shadow-md">
+            <div className="card-body space-y-4 p-6">
+              <h2 className="card-title m-0">Interact using your Smart Wallet!</h2>
+              <h2 className="text-lg m-0">Total Counter: {totalCounter?.toString() ?? 0}</h2>
+              <div className="space-y-2">
+                <h3 className="text-lg m-0">Set Greetings</h3>
+                <InputBase value={newGreeting} onChange={setGreeting} placeholder="LTMS..." />
+                <button className="btn btn-primary btn-sm" onClick={handleSetGreeting}>
+                  Send
+                </button>
+              </div>
+              <div className="space-y-2">
+                <h3 className="text-lg m-0">User Greeting Counter</h3>
+                <AddressInput value={counterAddress} onChange={setCounterAddress} placeholder="ENS or Address" />
+                <button className="btn btn-primary btn-sm" onClick={() => fetchUserGreetingCounter()}>
+                  Read
+                </button>
+                <p className="m-0">Counter Value: {userGreetingCounter?.toString() ?? 0} </p>
+              </div>
+            </div>
+          </div>
+          <div></div>
           <p className="text-center text-lg">
             Get started by editing{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
